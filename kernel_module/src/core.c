065e53f6eacc0cae55d10e172b7fc90f8e0adbdf
Rachit Shrivastava <rshriva@ncsu.edu>
2017-11-12 17:14:02 -0500

    Moving final version from ncsu github to my repo

---
+++ kernel_module/src/core.c
@@ -0,0 +1,165 @@
+//// Project 1: Mitkumar Pandya, mhpandya; Rachit Shrivastava, rshriva; Yash Vora, yvora;
+//////////////////////////////////////////////////////////////////////
+//                             North Carolina State University
+//
+//
+//
+//                             Copyright 2016
+//
+////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms and conditions of the GNU General Public License,
+// version 2, as published by the Free Software Foundation.
+//
+// This program is distributed in the hope it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+//
+////////////////////////////////////////////////////////////////////////
+//
+//   Author:  Hung-Wei Tseng
+//
+//   Description:
+//     Skeleton of NPHeap Pseudo Device
+//
+////////////////////////////////////////////////////////////////////////
+
+#include "npheap.h"
+
+#include <asm/uaccess.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/mm.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/poll.h>
+#include <linux/mutex.h>
+#include <linux/list.h>
+
+extern struct miscdevice npheap_dev;
+
+// this is the node structure of our linked list data structure
+struct node_list {
+	struct npheap_cmd cmd; // structure given in npheap.h
+	struct mutex lock; // mutex lock per node
+	//long offset;
+	unsigned long phys_addr; // allocated physical address in kernel memory
+	//unsigned long size;
+	struct list_head list; // kernel linked list head reference // https://isis.poly.edu/kulesh/stuff/src/klist/
+};
+
+// initializing pointers
+struct node_list ndlist;
+struct mutex lock;
+struct node_list *tmp;
+struct list_head *pos, *q;
+
+// method to find if node exists in linked list and is deleted or not
+int find(struct vm_area_struct *vma) {
+	// traversing linked list https://isis.poly.edu/kulesh/stuff/src/klist/
+	list_for_each_safe(pos, q, &ndlist.list) {
+		tmp= list_entry(pos, struct node_list, list);
+		// check if offset matches and size is not zero
+		// size zero indicates deleted node from kmemory
+		if (vma->vm_pgoff == tmp->cmd.offset && tmp->cmd.size > 0){
+			printk(KERN_INFO "found %zu %zu \n",tmp->cmd.offset, vma->vm_pgoff);
+			return 1; // found
+		}
+  	}
+  	return 0; // not found
+}
+
+int npheap_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+ // struct node_list pos;
+  //pos = ndlist;
+  int found = 0;
+  // variables to store size and allocated memory address
+  unsigned long phys_addr;
+  unsigned long size = vma->vm_end - vma->vm_start;
+
+  found = find(vma);
+  // if not found create a new node, allocate kernel memory and add it to linked list
+  if ( found == 0) {
+  	// allocating kernel memory https://www.kernel.org/doc/htmldocs/kernel-api/API-kmalloc.html
+	  void *kmemory = kmalloc(size, GFP_KERNEL);
+	  printk(KERN_INFO "Node %zu got: %zu bytes of memory\n",vma->vm_pgoff, ksize(kmemory));
+	  // find physical address using page_shift
+	  phys_addr = (unsigned long)virt_to_phys((void *)kmemory) >> PAGE_SHIFT;
+	  //	        printk(KERN_INFO "VMA offset: %zu", vma->vm_pgoff);
+
+	  int ret;
+	  // mapping allocated kernel memory to user virtual space address
+	  // http://www.makelinux.net/ldd3/chp-15-sect-2
+	  if((ret = remap_pfn_range(vma,
+			  vma->vm_start,
+			  phys_addr,
+			  size,
+			  vma->vm_page_prot)) < 0) {
+		  printk(KERN_ERR "ret is %d\n", ret);
+		  return ret;
+	  }
+	  //tmp = (struct node_list *)kmalloc(sizeof(struct node_list), GFP_KERNEL);
+	  //mutex_init(tmp->lock);
+	  //tmp->cmd.offset = vma->vm_pgoff;
+	  // assigning memory physical address and allocated size to linked list desired node
+	  	struct node_list *dummy;
+		list_for_each_safe(pos, q, &ndlist.list) {
+		    dummy = list_entry(pos, struct node_list, list);
+		    // if found assign values
+		    if(dummy->cmd.offset == vma->vm_pgoff) {
+		    	dummy->cmd.data = kmemory;
+				dummy->phys_addr = phys_addr;
+				dummy->cmd.size = size;
+				break;
+		    }
+		}
+	  //list_add(&(tmp->list), &(ndlist.list));
+
+  } else if (found == 1){
+  	// if node found, reassign it the previously allocated memory address and size
+	  int ret;
+	  printk(KERN_INFO "Actual size vs new size  %zu %zu ", tmp->cmd.size, size);
+	  if((ret = remap_pfn_range(vma,
+			  vma->vm_start,
+			  tmp->phys_addr,
+			  tmp->cmd.size,
+			  vma->vm_page_prot)) < 0) {
+		  printk(KERN_ERR "ret is %d\n", ret);
+		  return ret;
+	  }
+
+  }
+
+	return 0;
+}
+
+int npheap_init(void)
+{
+	int ret;
+	if ((ret = misc_register(&npheap_dev)))
+		printk(KERN_ERR "Unable to register \"npheap\" misc device\n");
+	else{
+		// initializing kernel linked list
+		// https://isis.poly.edu/kulesh/stuff/src/klist/
+		INIT_LIST_HEAD(&ndlist.list);
+		mutex_init(&lock);  // removed global lock
+		printk(KERN_ERR "\"npheap\" misc device installed\n");
+	}
+	return ret;
+}
+
+void npheap_exit(void)
+{
+    misc_deregister(&npheap_dev);
+}
+
---
+++ kernel_module/src/ioctl.c
@@ -0,0 +1,256 @@
+//// Project 1: Mitkumar Pandya, mhpandya; Rachit Shrivastava, rshriva; Yash Vora, yvora;
+//////////////////////////////////////////////////////////////////////
+//                             North Carolina State University
+//
+//
+//
+//                             Copyright 2016
+//
+////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms and conditions of the GNU General Public License,
+// version 2, as published by the Free Software Foundation.
+//
+// This program is distributed in the hope it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+//
+////////////////////////////////////////////////////////////////////////
+//
+//   Author:  Hung-Wei Tseng
+//
+//   Description:
+//     Skeleton of NPHeap Pseudo Device
+//
+////////////////////////////////////////////////////////////////////////
+
+#include "npheap.h"
+
+#include <asm/uaccess.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/mm.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/poll.h>
+#include <linux/mutex.h>
+#include <linux/list.h>
+
+// this is the node structure of our linked list data structure
+struct node_list {
+    struct npheap_cmd cmd; // structure given in npheap.h
+    struct mutex lock; // mutex lock per node
+    //long offset;
+    unsigned long phys_addr; // allocated physical address in kernel memory
+    //unsigned long size;
+    struct list_head list; // kernel linked list head reference // https://isis.poly.edu/kulesh/stuff/src/klist/
+};
+
+// accessing external variables defined in core.c file using extern 
+// https://en.wikipedia.org/wiki/External_variable 
+extern struct node_list ndlist;
+
+extern struct mutex lock;
+
+// check if node is already locked, return true: 1 , false:2, does not exist:0
+long is_locked(__u64 offset) {
+    struct node_list *tmp;
+    struct list_head *pos, *q;
+    list_for_each_safe(pos, q, &ndlist.list) {
+        tmp = list_entry(pos, struct node_list, list);
+        if((offset >> PAGE_SHIFT) == tmp->cmd.offset) {
+            if(tmp->cmd.op == 0) {
+                //found but locked
+                return 1;
+            } else if(tmp->cmd.op == 1) {
+                //found but unlocked
+                return 2;
+            }
+        }
+    }
+    return 0;
+}
+
+struct node_list* find_object(struct npheap_cmd cmd) {
+    //lock the existing node
+    struct node_list *tmp;
+    struct list_head *pos, *q;
+    // traverse through list and lock the current unocked node
+    // https://isis.poly.edu/kulesh/stuff/src/klist/
+    list_for_each_safe(pos, q, &ndlist.list) {
+        tmp = list_entry(pos, struct node_list, list);
+        if((cmd.offset >> PAGE_SHIFT) == tmp->cmd.offset) {
+            return tmp;
+        }
+    }
+    return NULL;
+}
+
+long npheap_lock(struct npheap_cmd __user *user_cmd)
+{
+    struct npheap_cmd cmd;
+    // copying user space struct to kernel 
+    //https://www.fsl.cs.sunysb.edu/kernel-api/re257.html
+    if(copy_from_user(&cmd, user_cmd, sizeof(*user_cmd))) {
+        return -1;
+    }
+    // check if node is locked
+    long isLock = is_locked(cmd.offset);
+    if(isLock == 0) {
+        // node does not exist, create one and lock it
+        //creade new node
+        struct node_list *tmp;
+        tmp = (struct node_list *)kmalloc(sizeof(struct node_list), GFP_KERNEL);
+        tmp->cmd.offset = cmd.offset >> PAGE_SHIFT;
+        tmp->cmd.op = 0;
+        tmp->cmd.size = 0;
+        printk("initialized and locked node %zu\n",tmp->cmd.offset);
+        mutex_init(&(tmp->lock)); // initializing current node mutex
+        mutex_lock(&(tmp->lock)); // lock current node mutex
+        list_add(&(tmp->list), &(ndlist.list)); // add current node to linked list
+        //https://isis.poly.edu/kulesh/stuff/src/klist/
+        return 1;   //pass
+    } else if(isLock == 2) {
+        //lock the existing node
+        struct node_list *tmp;
+        struct list_head *pos, *q;
+        // traverse through list and lock the current unocked node
+        // https://isis.poly.edu/kulesh/stuff/src/klist/
+        /*list_for_each_safe(pos, q, &ndlist.list) {
+            tmp = list_entry(pos, struct node_list, list);
+            if((cmd.offset >> PAGE_SHIFT) == tmp->cmd.offset) {
+                printk("node exists and unlocked, now locked %zu\n",tmp->cmd.offset);
+                tmp->cmd.op = 0;
+                mutex_lock(&(tmp->lock));
+                return 1;   //pass
+            }
+        }*/
+        tmp = find_object(cmd);
+        if(tmp != NULL) {
+            printk("node exists and unlocked, now locked %zu\n",tmp->cmd.offset);
+            tmp->cmd.op = 0;
+            mutex_lock(&(tmp->lock));
+            return 1;
+        }
+    }
+//    mutex_lock(&lock);
+    return 0;   //fail
+}     
+
+long npheap_unlock(struct npheap_cmd __user *user_cmd)
+{
+    struct npheap_cmd cmd;
+    if(copy_from_user(&cmd, user_cmd, sizeof(*user_cmd))) {
+        return -1;
+    }
+    struct node_list *tmp;
+    struct list_head *pos, *q;
+    // iterte through list and unlock the requested node
+    /*list_for_each_safe(pos, q, &ndlist.list) {
+        tmp = list_entry(pos, struct node_list, list);
+        //check if offset is same and it was locked before
+        if((cmd.offset >> PAGE_SHIFT) == tmp->cmd.offset && tmp->cmd.op == 0) {
+            tmp->cmd.op = 1;
+            printk("node %zu , unlocked\n", tmp->cmd.offset);
+            mutex_unlock(&(tmp->lock)); // unlocking current node
+            return 1;   //pass
+        }
+    }*/
+    tmp = find_object(cmd);
+    if(tmp != NULL && tmp->cmd.op == 0) {
+        tmp->cmd.op = 1;
+        printk("node %zu , unlocked\n", tmp->cmd.offset);
+        mutex_unlock(&(tmp->lock)); // unlocking current node
+        return 1;
+    }
+//    mutex_unlock(&lock);
+    return 0;   //fail
+}
+
+long npheap_getsize(struct npheap_cmd __user *user_cmd)
+{
+    struct npheap_cmd cmd;
+    if(copy_from_user(&cmd, user_cmd, sizeof(*user_cmd))) {
+        return -1;
+    }
+	struct node_list *tmp;
+	struct list_head *pos, *q;
+    // iterating through list to check if node associated with requested offset exists, if yes return it's size
+	/*list_for_each_safe(pos, q, &ndlist.list) {
+        tmp = list_entry(pos, struct node_list, list);
+        if ((cmd.offset >> PAGE_SHIFT) == tmp->cmd.offset){
+            return tmp->cmd.size; // return current node size
+        }
+	}*/
+    tmp = find_object(cmd);
+    if(tmp != NULL) {
+        return tmp->cmd.size;
+    }
+	return 0; // node not found, size is 0
+}
+
+long npheap_delete(struct npheap_cmd __user *user_cmd)
+{
+    struct npheap_cmd cmd;
+    if(copy_from_user(&cmd, user_cmd, sizeof(*user_cmd))) {
+        return -1;
+    }
+    struct node_list *tmp;
+    struct list_head *pos, *q;
+    // iterate through linked list and kfree data of associated offset node
+    /*list_for_each_safe(pos, q, &ndlist.list) {
+        tmp = list_entry(pos, struct node_list, list);
+        //check if offset is same and its unlocked
+        // if found and is unlocked
+        if((cmd.offset >> PAGE_SHIFT) == tmp->cmd.offset && tmp->cmd.op == 0) {
+            //Delete Code
+	        printk(KERN_INFO "deleting node %zu\n",tmp->cmd.offset);
+	        //list_del(pos);
+            // make size 0
+	        tmp->cmd.size = 0;
+            // free associated memory from kernel memory space
+            // https://www.kernel.org/doc/htmldocs/kernel-api/API-kfree.html
+            kfree(tmp->cmd.data); 
+            tmp->cmd.data = NULL;
+            return 1;
+        }
+    }*/
+    tmp = find_object(cmd);
+    if(tmp != NULL && tmp->cmd.op == 0) {
+        //Delete Code
+        mutex_lock(&lock);
+        printk(KERN_INFO "deleting node %zu\n",tmp->cmd.offset);
+        tmp->cmd.size = 0;
+        kfree(tmp->cmd.data); 
+        tmp->cmd.data = NULL;
+        mutex_unlock(&lock);
+        return 1;
+    }
+    return 0;
+}
+
+long npheap_ioctl(struct file *filp, unsigned int cmd,
+                                unsigned long arg)
+{
+    switch (cmd) {
+    case NPHEAP_IOCTL_LOCK:
+        return npheap_lock((void __user *) arg);
+    case NPHEAP_IOCTL_UNLOCK:
+        return npheap_unlock((void __user *) arg);
+    case NPHEAP_IOCTL_GETSIZE:
+        return npheap_getsize((void __user *) arg);
+    case NPHEAP_IOCTL_DELETE:
+        return npheap_delete((void __user *) arg);
+    default:
+        return -ENOTTY;
+    }
+}
